001872878a55d17069f6c080debf6abdecd505c7
Luis Lopez <llopez@cloudbit.com.bo>
2018-06-21 18:35:45 -0400

    Initial configuration

--- express-app/app.js
+++ express-app/app.js
@@ -1,15 +1,18 @@
 const express = require("express"),
-  path = require("path"),
-  //favicon = require('serve-favicon'),
-  logger = require("morgan"),
-  cookieParser = require("cookie-parser"),
-  bodyParser = require("body-parser"),
-  routes = require("./routes"),
-  app = express();
+	path = require("path"),
+	//favicon = require('serve-favicon'),
+	logger = require("morgan"),
+	cookieParser = require("cookie-parser"),
+	bodyParser = require("body-parser"),
+	app = express();
+
+//Controllers
+var indexController = require('./controllers/index');
+var userController = require('./controllers/user');
 
 //view engine setup
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "hbs");
+app.set("view engine", "pug");
 
 //uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -19,14 +22,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", routes);
+app.use('/', indexController);
+app.use('/user', userController);
 
 //catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  const err = new Error("Not Found");
-
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+	const err = new Error("Not Found");
+	err.status = 404;
+	next(err);
 });
 
 //error handlers
@@ -34,23 +37,23 @@ app.use(function(req, res, next) {
 //development error handler
 //will print stacktrace
 if (app.get("env") === "development") {
-  app.use(function(err, req, res) {
-    res.status(err.status || 500);
-    res.render("error", {
-      message: err.message,
-      error: err
-    });
-  });
+	app.use(function (err, req, res) {
+		res.status(err.status || 500);
+		res.render("error", {
+			message: err.message,
+			error: err
+		});
+	});
 }
 
 //production error handler
 //no stacktraces leaked to user
-app.use(function(err, req, res) {
-  res.status(err.status || 500);
-  res.render("error", {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res) {
+	res.status(err.status || 500);
+	res.render("error", {
+		message: err.message,
+		error: {}
+	});
 });
 
 module.exports = app;
--- main.js
+++ main.js
@@ -1,38 +1,38 @@
 const electron = require("electron"),
-  app = electron.app,
-  BrowserWindow = electron.BrowserWindow;
+	app = electron.app,
+	BrowserWindow = electron.BrowserWindow;
 
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({
-    autoHideMenuBar: true,
-    width: 640,
-    height: 480
-  });
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
-  //mainWindow.webContents.openDevTools();
-  mainWindow.on('close', () => {
-    mainWindow.webContents.send('stop-server');
-  });
-  mainWindow.on("closed", () => {
-    mainWindow = null;
-  });
+	mainWindow = new BrowserWindow({
+		autoHideMenuBar: true,
+		width: 800,
+		height: 600
+	});
+	mainWindow.loadURL(`file://${__dirname}/index.html`);
+	//mainWindow.webContents.openDevTools();
+	mainWindow.on('close', () => {
+		mainWindow.webContents.send('stop-server');
+	});
+	mainWindow.on("closed", () => {
+		mainWindow = null;
+	});
 }
 
 app.on("ready", createWindow);
 app.on("browser-window-created", function (e, window) {
-  window.setMenu(null);
+	window.setMenu(null);
 });
 
 app.on("window-all-closed", function () {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+	if (process.platform !== "darwin") {
+		app.quit();
+	}
 });
 
 app.on("activate", function () {
-  if (mainWindow === null) {
-    createWindow();
-  }
+	if (mainWindow === null) {
+		createWindow();
+	}
 });
